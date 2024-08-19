7f28ed8a344891eb2da2d0fa77a2ec56803bc647
Ahsan Zafar <152809262+Rizali7@users.noreply.github.com>
2024-08-19 23:02:32 +0500

    updated
    updated

---
+++ backend/dist/services/application.service.d.ts
@@ -0,0 +1,7 @@
+import { Application } from 'src/models/applications/application.entity';
+import { ApplicationDto } from 'src/api/brokers/applications/list-applications/list-applications.dto';
+export declare class ApplicationService {
+    private applicationRepository;
+    constructor(applicationRepository: typeof Application);
+    createApplication(dto: ApplicationDto): Promise<Application>;
+}
